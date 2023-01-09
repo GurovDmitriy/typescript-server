3c9a5b9dd0c5e8a67da6a7581d8d8e651c3b437b
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2023-01-09 22:39:30 +0300

    feat: body parser

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -2,8 +2,28 @@ import { Router } from "express"
 
 const router = Router()
 
-router.get("/", (req, res) => {
-  res.send("hi here")
+router.get("/login", (req, res) => {
+  res.send(`
+    <form method="POST">
+        <div>
+            <div>
+                <label for="email-field">Email</label>
+                <input type="email" id="email-field">
+            </div>
+            <div>
+                <label for="password-field">Password</label>
+                <input type="password" id="password-field">
+            </div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+  `)
+})
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body
+
+  res.sendStatus(200)
 })
 
 export { router }
