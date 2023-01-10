9499313b5b13d02049c7c26446195aa34a866d62
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2023-01-10 21:55:36 +0300

    feat: protected route

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,18 +1,57 @@
-import { Router } from "express"
+import { Router, Request, Response, NextFunction } from "express"
+
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined
+  }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req?.session?.loggedIn) {
+    next()
+    return
+  }
+
+  res.status(403)
+  res.send(`
+    <div>
+        <div>Not permitted</div>
+        <a href="/">Go home</a>
+    </div>
+  `)
+}
 
 const router = Router()
 
+router.get("/", (req, res) => {
+  if (req?.session?.loggedIn) {
+    res.send(`
+    <div>
+        <div>You are login</div>
+        <a href="/logout">Logout</a>
+    </div>
+  `)
+  } else {
+    res.send(`
+    <div>
+        <div>You are not login</div>
+        <a href="/login">Login</a>
+    </div>
+  `)
+  }
+})
+
 router.get("/login", (req, res) => {
   res.send(`
-    <form method="POST">
+    <form method="POST" name="form-login">
         <div>
             <div>
                 <label for="email-field">Email</label>
-                <input type="email" id="email-field">
+                <input type="email" id="email-field" name="email">
             </div>
             <div>
                 <label for="password-field">Password</label>
-                <input type="password" id="password-field">
+                <input type="password" id="password-field" name="password">
             </div>
             <button type="submit">Submit</button>
         </div>
@@ -20,10 +59,33 @@ router.get("/login", (req, res) => {
   `)
 })
 
-router.post("/login", (req, res) => {
+router.post("/login", (req: RequestWithBody, res) => {
   const { email, password } = req.body
 
-  res.sendStatus(200)
+  console.log(req.body)
+
+  const emailValid = email && email === "test@gmail.com"
+  const passwordValid = email && password === "12345"
+
+  if (emailValid && passwordValid) {
+    req.session = { loggedIn: true }
+    res.redirect("/")
+  } else {
+    res.send("Invalid email or password")
+  }
+})
+
+router.get("/logout", (req, res) => {
+  req.session = undefined
+  res.redirect("/")
+})
+
+router.get("/protected", requireAuth, (req, res) => {
+  res.send(`
+    <div>
+        <div>Wilcome to protected route, logged in user</div>
+    </div>
+  `)
 })
 
 export { router }
